b5e43ba91fa292e55646c5f180122ba387489250
Ascii175 <slimeyily@gmail.com>
2021-04-09 17:56:03 +0700

    Msg update

---
+++ HTML - Scorilo 1.0.1/PHP/Login_Register.php
@@ -0,0 +1,5 @@
+<?php
+$name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
+$email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
+$comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
+?>
